8991e6efa2f7b8d5094ab1f5df9c45d68a3bbce0
Maria Eduarda <mariaeduardasoter@gmail.com>
2023-04-06 18:43:18 -0300

    Remessa de exercícios.

---
+++ src/resolucaoIniciante/QuadradoDePares_1073.java
@@ -0,0 +1,20 @@
+package resolucaoIniciante;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class QuadradoDePares_1073 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + "^2" +" = " + i*i);
+			}
+		}
+	}
+}
