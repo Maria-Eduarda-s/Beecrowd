81c09a7040b13fc3f85449032bb704b3c65891a2
Maria Eduarda <mariaeduardasoter@gmail.com>
2025-01-03 23:59:06 -0300

    new exercises

---
+++ src/resolucaoiniciante/AArvoreDeNatal_3040.java
@@ -0,0 +1,14 @@
+package resolucaoiniciante;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AArvoreDeNatal_3040 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
