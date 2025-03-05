5531ad61bb5f46b74a4fca945cbe5c9599aa427c
Maria Eduarda <mariaeduardasoter@gmail.com>
2025-03-04 23:56:52 -0300

    New exercise

---
+++ java/src/resolucaoiniciante/VivaACiencia_3157.java
@@ -0,0 +1,35 @@
+package resolucaoiniciante;
+
+import java.util.Scanner;
+
+/*
+Oronob é um garoto muito peculiar. Para ele, apenas a sua opinião importa e nada mais. Durante a pandemia do Coronga Virus, ele usa sua opinião para retaliar o que a ciência diz. 
+Ele então, para impulsionar suas opiniões, criou um bot (robô que gera mensagens automáticas) para disparar seus textos para muitas pessoas.
+
+Você conhece Oronob, porém, ao contrário dele, você é sensato e sabe que ciência não se refuta com opinião, além também de entender de programação, então decidiu hackear o bot de 
+Oronob para evitar suas falácias.
+
+Pois bem, crie então um algoritmo que muda o texto das opiniões de Oronob para a real verdade.
+
+Entrada
+Um texto S com caracteres alfanuméricos e possíveis espaços entre as palavras.
+
+Limites: 0 < |S| <= 280. ;)
+
+Saída
+A verdade.
+ */
+
+public class VivaACiencia_3157 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println("Nao se refuta ciencia com opiniao");
+
+        sc.close();
+    }
+}
